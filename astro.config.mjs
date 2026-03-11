4934515ac42af239f131ce6b722e174e6f90686c
kenandotfyi <kenan.akinci@icloud.com>
2026-03-11 20:19:53 +0100

    astro upgrade, fonts api, expressive code settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import { remarkReadingTime } from "./src/lib/remark-readtime.mjs";
-import { existsSync, mkdirSync, readdirSync, copyFileSync } from "fs";
+import { existsSync, mkdirSync, readFileSync, readdirSync, copyFileSync } from "fs";
 import { join } from "path";
 
 import rehypeKatex from "rehype-katex";
 import rehypeExternalLinks from "rehype-external-links";
 import rehypeSidenotes from "./src/lib/rehype-sidenotes.mjs";
 import rehypeHrDivider from "./src/lib/rehype-hr-divider.mjs";
-import expressiveCode from "astro-expressive-code";
+import expressiveCode, { ExpressiveCodeTheme } from "astro-expressive-code";
 import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
 import mdx from "@astrojs/mdx";
 
@@ -16,11 +16,41 @@ import svelte from "@astrojs/svelte";
 
 import icon from "astro-icon";
 
+
+const darkFile = readFileSync(new URL(`./src/assets/code-themes/dark.jsonc`, import.meta.url), 'utf-8')
+const lightFile = readFileSync(new URL(`./src/assets/code-themes/light.jsonc`, import.meta.url), 'utf-8')
+
+const darkTheme = ExpressiveCodeTheme.fromJSONString(darkFile)
+const lightTheme = ExpressiveCodeTheme.fromJSONString(lightFile)
+
+
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://kenan.fyi",
   output: "static",
   scopedStyleStrategy: "class",
+  fonts: [{
+    provider: fontProviders.local(),
+    name: "Inter Variable",
+    cssVariable: "--font-sans",
+    options: {
+      variants: [{
+        src: ['./src/assets/fonts/InterVariable.woff2'],
+      }]
+    }
+  }, {
+    provider: fontProviders.local(),
+    name: "JetBrains Mono",
+    cssVariable: "--font-mono",
+    options: {
+      variants: [{
+        src: ['./src/assets/fonts/JetBrainsMono-Regular.woff2'],
+      }]
+    }
+  }
+
+  ],
   integrations: [
     // since Vite copies the public/ to dist/ before page frontmatter runs.
     // files in public/covers during rendering never copied there.
@@ -47,24 +77,20 @@ export default defineConfig({
       frames: {
         showCopyToClipboardButton: false,
       },
-      shiki: {
-      },
-      themes: ["vesper", "github-light"],
-      useDarkModeMediaQuery: false,
+      themes: [darkTheme, lightTheme],
       themeCssSelector: (theme) =>
-        theme.type === "light"
-          ? '[data-theme="light"]'
-          : ':not([data-theme="light"])',
+        `[data-theme='${theme.type}']`,
       styleOverrides: {
         borderRadius: "0px",
-        codeFontFamily: "JetBrains Mono",
-        uiFontFamily: "JetBrains Mono",
+        codeFontFamily: "var(--font-mono)",
+        uiFontFamily: "var(--font-mono)",
         codeFontSize: "14px",
         uiFontSize: "13px",
         uiFontWeight: "bold",
         frames: {
           frameBoxShadowCssValue: "none",
-
+          editorActiveTabIndicatorTopColor: "none",
+          editorActiveTabIndicatorBottomColor: "none",
         },
         textMarkers: {
           markBackground: "gainsboro",
